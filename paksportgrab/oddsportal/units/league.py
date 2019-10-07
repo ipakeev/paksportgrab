c711e761eab8de38c6afddcb0f20489bd20dec4a
Ipakeev <ipakeev93@gmail.com>
2019-10-07 17:19:28 +0300

    0.1.5

--- paksportgrab/oddsportal/units/league.py
+++ paksportgrab/oddsportal/units/league.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from .border import Border
 from .match import Match
@@ -8,6 +8,7 @@ class League(object):
     sport: str
     country: str
     league: str
+    leagueId: Optional[int]
     leagueUrl: str
     nextMatches: List[Match]
     seasons: List[str]
@@ -16,6 +17,7 @@ class League(object):
         self.sport = border.sport
         self.country = border.country
         self.league = border.league
+        self.leagueId = None
         self.leagueUrl = border.leagueUrl
         self.nextMatches = []
 
@@ -23,6 +25,7 @@ class League(object):
         m.sport = self.sport
         m.country = self.country
         m.league = self.league
+        m.leagueId = self.leagueId
 
     def addNextMatch(self, m: Match):
         self.setToMatch(m)
