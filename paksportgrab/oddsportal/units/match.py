c4904495ef0d75c7273607e1473d1f263a83f2b6
Ipakeev <ipakeev93@gmail.com>
2020-01-07 22:24:17 +0300

    0.3.0 Added dateTime

--- paksportgrab/oddsportal/units/match.py
+++ paksportgrab/oddsportal/units/match.py
@@ -32,6 +32,10 @@ class Match:
     filledOdds: bool
     filledScore: bool
 
+    madeNextAt: datetime.datetime
+    madeReadyAt: datetime.datetime
+    updatedAt: datetime.datetime
+
     data: dict
     wdlName: str
     scoreName: str
